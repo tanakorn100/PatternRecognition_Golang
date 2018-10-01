f65db0639410bced8dbbd3e498c3578828d60359
Tanakorn Puraram <tanakorn.pur@bru.ac.th>
2018-10-01 09:51:16 +0700

    regexp

--- PatternRecognition_email_regularexpression.go
+++ PatternRecognition_email_regularexpression.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 )
 
 func main() {
-	fmt.Println("hello world")
+	r, err := regexp.Compile(`Nattawut`)
+
+	if err != nil {
+		fmt.Printf("Error : Don't have Nattawut")
+		return
+	}
+
+	if r.MatchString("Hello Regular Expression. from Nattawut") == true {
+		fmt.Printf("Match ")
+	} else {
+		fmt.Printf("No match ")
+	}
+
+	r2, err := regexp.Compile(`\d`)
+	if err != nil {
+		fmt.Printf("Error : Type not INT")
+		return
+	}
+
+	// Will print 'true':
+	fmt.Printf("%v", r2.MatchString("Seven times seven is 49."))
+	// Will print 'false':
+	fmt.Printf("%v", r2.MatchString("Seven times seven is forty-nine."))
+
 }
