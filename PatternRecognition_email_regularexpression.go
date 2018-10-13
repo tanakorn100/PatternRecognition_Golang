35fc9a351244fc9ddbf62d0d3194d9fa3211c601
Tanakorn Puraram <tanakorn.pur@bru.ac.th>
2018-10-13 14:01:57 +0700

    training file

--- PatternRecognition_email_regularexpression.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Kor Computer Science KMITL")
-}
---
+++ trainingGo.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Kor Computer Science KMITL")
+
+	var number int = 200
+	number2 := 500 // automatic assign type of variable
+	fmt.Println("The number in this project is : ", number)
+	fmt.Println("The number which automatic assign is : ", number2)
+}
