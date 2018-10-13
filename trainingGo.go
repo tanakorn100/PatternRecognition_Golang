6e470addb4515b91424cdf7922ec121e59468b0e
Tanakorn Puraram <tanakorn.pur@bru.ac.th>
2018-10-13 16:29:27 +0700

    training

--- trainingGo.go
+++ trainingGo.go
@@ -6,13 +6,73 @@ import "fmt"
 var globalVariable int = 5000
 
 func main() {
+	x := 15
+	fmt.Println("value x is ", x)
+	fmt.Println("Address x is ", &x)
+	var p *int
+	p = &x //pointer
+	fmt.Println("Pointer p is %x", p)
+	*p = 20 //เปลี่ยนค่าให้เป็น 20 ด้วย pointer
+	fmt.Println("value x is ", x)
+
+	closure()
+	mp()
+	slicing()
 	array()
 	summation(1, 2, 3) //data
 	fmt.Println(factorial(5))
 }
 
-func slicing() { // don't fix len of array
+func structure() {
+	//like database use to store data
 
+}
+func closure() {
+	add := func(x, y int) int {
+		return x + y
+	}
+	fmt.Println(add(1, 2))
+
+	x := 0
+	increment := func() int {
+		x++
+		return x
+	}
+	fmt.Println(increment())
+}
+
+func slicing() { // don't fix len of array
+	// x := make([]int, 5)
+	// fmt.Println(x)
+
+	//second for
+	slice := []float64{1, 2, 3}
+	fmt.Println(slice)
+	slice2 := append(slice, 4, 5)
+	fmt.Println(slice2)
+
+	//copy
+	sliceA := []int{1, 2, 3}
+	sliceB := make([]int, 2)
+	copy(sliceB, sliceA)
+	fmt.Println(sliceB)
+}
+
+func mp() {
+	// like a dictionary of python's type : key , value
+	mp := make(map[string]string)
+	mp["TH"] = "THAILAND"
+	mp["SG"] = "SINGAPORE"
+	mp["JP"] = "JAPAN"
+
+	fmt.Println(mp["JP"])
+
+	smp := map[string]string{
+		"TH": "THAILAND",
+		"SG": "SINGAPORE",
+		"JP": "JAPAN",
+	}
+	fmt.Println(smp)
 }
 func array() {
 	var x [5]int
