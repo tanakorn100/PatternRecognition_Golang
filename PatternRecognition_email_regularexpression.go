430ff6ea898e2feec31ac35d150bf840788f21ef
Tanakorn Puraram <tanakorn.pur@bru.ac.th>
2018-10-21 09:01:06 +0700

    print data

--- PatternRecognition_email_regularexpression.go
+++ PatternRecognition_email_regularexpression.go
@@ -13,60 +13,100 @@ func checkerror(e error) {
 	}
 }
 
+var sender_email_array [5]string
+var sender_name_array [5]string
+var username_array [5]string
+var domainname_array [5]string
+var recipient_email_array [5]string
+var datesend_array [5]string
+var subject_array [5]string
+var contentemail_array [5]string
+
 func main() {
-	data, err := ioutil.ReadFile("emails.txt")
+	data, err := ioutil.ReadFile("fradulent_emails.txt")
 	checkerror(err)
-	// convert data from bytes type to strings
 	emails_data_str := string(data)
-	// split email dataset to array list
 	emails := s.Split(emails_data_str, "From r ")
-	// remove index 0 is blank
 	emails = emails[1:]
 	fmt.Println("Total : ", len(emails))
 
 	for index, email := range emails {
 
-		fmt.Println("----- Email No. ", index+1, " ------")
+		// fmt.Println("----- Email No. ", index+1, " ------")
+
 		sender := regexp.MustCompile("From:.*")
 		sender_data := sender.FindString(email)
 
 		s_email := regexp.MustCompile("<.*@[a-zA-Z]+.[a-zA-Z]+")
 		email_sender := s_email.FindString(sender_data)
+		emails_sender := s.Trim(email_sender, "<")
 
 		s_name := regexp.MustCompile("\".*\"")
 		name_sender := s_name.FindString(sender_data)
+		names_sender := s.Trim(name_sender, "\"")
 
-		fmt.Println("Email-Sender : ", email_sender[1:])
-		fmt.Println("Name-Sender : ", s.Trim(name_sender, "\""))
-		email_account := s.Split(email_sender[1:], "@")
-		fmt.Println("Account : ", email_account[0], "\tDomain : ", email_account[1])
+		//fmt.Println("Sender Email : ", emails_sender)
+		//fmt.Println("Sender Name : ", names_sender)
+
+		sender_email_array[index] = emails_sender
+		sender_name_array[index] = names_sender
+
+		email_account := s.Split(emails_sender, "@")
+		//fmt.Println("Username : ", email_account[0], "\tDomain : ", email_account[1])
+		username_array[index] = email_account[0]
+		domainname_array[index] = email_account[1]
 
 		recipient := regexp.MustCompile("To:.*")
 		recipient_data := recipient.FindString(email)
 		email_rep := regexp.MustCompile(" .*@.*$")
 		recipient_email := email_rep.FindString(recipient_data)
+
+		recipient_email_array[index] = recipient_email
+
 		//fmt.Println("Name-Recipient : ", s.Replace(recipient_email, " ", "", -1))
-		fmt.Println("Name-Recipient :", recipient_email)
+		//fmt.Println("Recipient Email :", recipient_email)
 
 		date := regexp.MustCompile("Date:.*")
 		date_data := date.FindString(email)
 		date_re := regexp.MustCompile("[0-9]+.[a-zA-Z]+.[0-9]+.")
 		date_my := date_re.FindString(date_data)
-		fmt.Println("Date send : ", date_my)
+
+		datesend_array[index] = date_my
+		//fmt.Println("Date send : ", date_my)
 
 		subject := regexp.MustCompile("Subject:[\t\n\f\r ]*.*")
 		subject_data := subject.FindString(email)
+
+		subject_array[index] = subject_data
 		//subject_title := s.Replace(subject_data, "Subject: ", "", -1)
-		fmt.Println(subject_data)
+		//fmt.Println(subject_data)
 
-		content := regexp.MustCompile("Status:[\t\n\f\r ]+[a-zA-Z]+(\n|[0-9A-Za-z_]+|.*|[\t\n\f\r ])+")
+		content := regexp.MustCompile("Status:[\t\n\f\r ]+[a-zA-Z0-9]+(\n|[0-9A-Za-z_]+|.*|[\t\n\f\r ])+")
 		content_data := content.FindString(email)
-		fmt.Println("Body : ", content_data)
 
-		// data := map[string]string{
-		// 	"Email_sender": email_sender[1:],
-		// }
-		// fmt.Println(data)
+		contentemail_array[index] = content_data
+		//fmt.Println("Email-Content : ", content_data)
+
 	}
 
+	// fmt.Println(sender_email_array)
+
+	// var email_datas [][]array
+
+	for index, _ := range sender_email_array {
+		email := map[string]string{
+			"sender_email":    sender_email_array[index],
+			"sender_name":     sender_name_array[index],
+			"username":        username_array[index],
+			"domainname":      domainname_array[index],
+			"recipient_email": recipient_email_array[index],
+			"datesent":        datesend_array[index],
+			"subject":         subject_array[index],
+			"contentemail":    contentemail_array[index],
+		}
+
+		fmt.Println(email["domainname"])
+
+		// email_datas[index] = email
+	}
 }
