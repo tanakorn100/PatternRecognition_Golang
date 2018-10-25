aced96e3b125b55273de7a47aee44b9621cd4d2f
Tanakorn Puraram <tanakorn.pur@bru.ac.th>
2018-10-25 09:54:11 +0700

    new code recipient data

--- fradulent_emails.go
+++ fradulent_emails.go
@@ -14,6 +14,10 @@ func checkerror(e error) {
 }
 
 var sender_email_array [3976]string
+var sender_name_array [3976]string
+var username_array [3976]string
+var domainname_array [3976]string
+var recipient_email_array [3976]string
 
 func main() {
 	data, err := ioutil.ReadFile("fradulent_emails_dataset.txt")
@@ -30,28 +34,53 @@ func main() {
 		sender := regexp.MustCompile("From:.*")
 		sender_data := sender.FindString(email)
 
-		fmt.Println(sender_data)
-
-		s_email := regexp.MustCompile("<.*@[a-zA-Z]+.[a-zA-Z]+")
+		s_email := regexp.MustCompile("<.*[^\t\n\f\r ]@[a-zA-Z]+.[a-zA-Z]+")
 		email_sender := s_email.FindString(sender_data)
 		emails_sender := s.Trim(email_sender, "<")
-		fmt.Println(emails_sender)
+
 		if emails_sender != "" {
 			sender_email_array[index] = emails_sender
+			email_account := s.Split(emails_sender, "@")
+			username_array[index] = email_account[0]
+			domainname_array[index] = email_account[1]
 		} else {
 			sender_email_array[index] = "None"
+			username_array[index] = "None"
+			domainname_array[index] = "None"
 		}
+
 		s_name := regexp.MustCompile("From:[\t\n\f\r ]+.*[\t\n\f\r ]+")
 		name_sender := s_name.FindString(sender_data)
 		name_sender = s.Replace(name_sender, "From: ", "", -1)
+
+		// remore \" text \"
+
 		// if name_sender[0] == '"' {
 		// 	name_sender = s.Trim(name_sender, "\"*\"")
 		// }
-		fmt.Println(name_sender)
 
+		// append name_sender to array list
+		if name_sender != "" {
+			sender_name_array[index] = name_sender
+		} else {
+			sender_name_array[index] = "None"
+		}
+
+		recipient := regexp.MustCompile("\nTo:.*")
+		recipient_data := recipient.FindString(email)
+		recipient_data = s.Replace(recipient_data, "\nTo: ", "", -1)
+		fmt.Println(recipient_data)
+		if recipient_data != "" {
+			recipient_email_array[index] = recipient_data
+		} else {
+			recipient_email_array[index] = "None"
+		}
 	}
 
 	//fmt.Println(sender_email_array)
 	fmt.Println(len(sender_email_array))
+	//fmt.Println(sender_name_array)
+	fmt.Println(len(sender_name_array))
+	fmt.Println(recipient_data)
 
 }
